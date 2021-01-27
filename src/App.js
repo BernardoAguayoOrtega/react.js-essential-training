c0e2010626d34fad3224d1a4c95124415377a9fc
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-27 10:27:10 -0600

    [update] conditional rendering first step

--- src/App.js
+++ src/App.js
@@ -1,51 +1,19 @@
 import './App.css';
-import restaurant from './restaurant/restaurant.jpeg'
 
-function Header(props) {
-
-  return(
-    <header>
-      <h1>{props.name}'s blog</h1>
-    </header>
-  )
+function SecretComponent() {
+	return <h1>Secret Information</h1>;
 }
 
-function Content(props) {
-
-  return(
-    <section>
-      <p>We serve the most {props.adjective} food around.</p>
-      <img src={restaurant} alt="restaurant"/>
-      <ul style={{textAlign: "left"}}>
-        {props.dishes.map((dish) => <li key={dish.index}>{dish.dish}</li>)}
-      </ul>
-    </section>
-  )
+function RegularComponent() {
+	return <h1>Everybody can see this</h1>;
 }
 
-function Footer(props) {
-
-  return(
-    <footer>Copyright {props.year}</footer>
-  )
-}
-
-const dishes = [
-  "Macaroni and Cheese",
-  "Salmon",
-  "Tofu with Vegetables"
-]
-
-const dishesObjects = dishes.map((dish, index) => ({dish, index}))
-
-function App() {
-  return (
-    <div className="App">
-      <Header name="Bernie"/>
-      <Content adjective='Amazing' dishes={dishesObjects} />
-      <Footer year={new Date().getFullYear()}/>
-    </div>
-  );
+function App(props) {
+	if (props.authorized) {
+    return <SecretComponent />
+	} else {
+    return <RegularComponent />
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'
 
-function AppTwo() {
-  return <h1>This is the second app</h1>
-}
-
 ReactDOM.render(
-  <>
-  <App />
-  <AppTwo />
-  </>,
+  <App authorized={false}/>,
   document.getElementById('root')
 );
