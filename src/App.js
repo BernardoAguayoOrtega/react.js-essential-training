86b3a20a33b5e17ce55044331a05a8e8111726ba
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-02-02 21:30:38 -0600

    [add] is loading

--- src/App.js
+++ src/App.js
@@ -5,20 +5,28 @@ import './App.css';
 
 export default function App() {
 	const [data, setData] = useState(null);
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
+		setLoading(true);
+
 		fetch('https://api.github.com/users/BernardoAguayoOrtega')
 			.then((response) => response.json())
-			.then(setData);
-	});
-	
-	if (data) {
-		return <div>
+			.then(setData)
+			.then(() => setLoading((value) => (value = false)))
+			.catch(setError);
+	}, []);
+
+	if (loading) return <h1>Is loading</h1>;
+
+	if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+
+	return (
+		<div>
 			<h1>{data?.name}</h1>
 			<p>{data?.location}</p>
 			<img alt={data?.login} src={data?.avatar_url} />
 		</div>
-	}
-
-	return <>we do not have data</>;
+	);
 }
